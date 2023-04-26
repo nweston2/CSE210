ebdde6d67a3f42c07b87e76f4c713a8bb920236d
Noah Weston <weston4ever@gmail.com>
2023-04-26 16:24:46 -0600

    prep5 complete

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,47 @@
 using System;
 
-class Program
+namespace prep5
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void Main(string[] args)
+        {
+            displayWelcome();
+            string nwName = promptUserName();
+            int nwFaveNum = promptUserNumber();
+            int nwSquare = squareNumber(nwFaveNum);
+            displayResults(nwSquare, nwName);
+        }
+
+        static void displayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string promptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string nwName = Console.ReadLine();
+            return nwName;
+        }
+
+        static int promptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string nwNumStr = Console.ReadLine();
+            int nwNum = int.Parse(nwNumStr);
+            return nwNum;
+        }
+
+        static int squareNumber(int nwFav)
+        {
+            int nwSquared = nwFav * nwFav;
+            return nwSquared;
+        }
+
+        static void displayResults(int num, string username)
+        {
+            Console.WriteLine($"{username}, the square of your number is {num}.");
+        }
     }
 }
