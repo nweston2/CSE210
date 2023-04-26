37aa46ce41c83141590b1d10579a572d85db34b3
Noah Weston <weston4ever@gmail.com>
2023-04-26 09:36:16 -0600

    prep3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,7 +6,34 @@ namespace prep3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Prep3 World!");
+            Random randomGenerator = new Random();
+            int nwAnswer = randomGenerator.Next(1,100);
+            int nwGuess;
+            int nwAttempts = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                string nwGuessStr = Console.ReadLine();
+                nwGuess = int.Parse(nwGuessStr);
+
+                if (nwGuess != nwAnswer)
+                {
+                    if (nwGuess > nwAnswer)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+
+                    else if (nwGuess < nwAnswer)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+
+                    nwAttempts += 1;
+                }
+            } while (nwGuess != nwAnswer);
+
+            Console.WriteLine($"You got it! It took you {nwAttempts} tries.");
         }
     }
 }
