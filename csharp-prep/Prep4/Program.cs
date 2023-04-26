4777c8a559109aaa750cc23ba6c410323355ad48
Noah Weston <weston4ever@gmail.com>
2023-04-26 10:02:32 -0600

    26 Apr classwork

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
+using System.Collections.Generic;
 using System;
 
-class Program
+namespace prep4
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep4 World!");
+        static void Main(string[] args)
+        {
+            List<int> nwNumbers = new List<int>();
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            int nwNum;
+            string nwNumStr;
+
+            do
+            {
+                Console.Write("Enter number: ");
+                nwNumStr = Console.ReadLine();
+                nwNum = int.Parse(nwNumStr);
+
+                if (nwNum != 0)
+                {
+                    nwNumbers.Add(nwNum);
+                }
+            } while (nwNum != 0);
+
+            int nwSum = 0;
+            int nwBiggest = 0;
+
+            foreach (int nwNumber in nwNumbers)
+            {
+
+            }
+        }
     }
 }
