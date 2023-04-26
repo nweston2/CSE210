cf1b522aa1c345ccf799ceb19db95c6b5d0f4c22
Noah Weston <weston4ever@gmail.com>
2023-04-26 15:52:55 -0600

    prep4 done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -7,16 +7,16 @@ namespace prep4
     {
         static void Main(string[] args)
         {
-            List<int> nwNumbers = new List<int>();
+            List<float> nwNumbers = new List<float>();
             Console.WriteLine("Enter a list of numbers, type 0 when finished.");
-            int nwNum;
+            float nwNum;
             string nwNumStr;
 
             do
             {
                 Console.Write("Enter number: ");
                 nwNumStr = Console.ReadLine();
-                nwNum = int.Parse(nwNumStr);
+                nwNum = float.Parse(nwNumStr);
 
                 if (nwNum != 0)
                 {
@@ -24,13 +24,24 @@ namespace prep4
                 }
             } while (nwNum != 0);
 
-            int nwSum = 0;
-            int nwBiggest = 0;
+            float nwSum = 0;
+            float nwBiggest = 0;
 
-            foreach (int nwNumber in nwNumbers)
+            foreach (float nwNumber in nwNumbers)
             {
-
+                nwSum += nwNumber;
+                if (nwNumber > nwBiggest)
+                {
+                    nwBiggest = nwNumber;
+                }
+                // Console.WriteLine(nwNumber);
             }
+            float nwAmount = nwNumbers.Count;
+            float nwAverage = nwSum / nwAmount;
+
+            Console.WriteLine($"The sum is: {nwSum}");
+            Console.WriteLine($"The average is: {nwAverage}");
+            Console.WriteLine($"The largest number is: {nwBiggest}");
         }
     }
 }
