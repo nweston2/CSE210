185c31d66d148cdc78263b2ee1384d2db8daa504
Noah Weston <weston4ever@gmail.com>
2023-05-31 09:59:10 -0600

    in class 31 may 23

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,26 +6,28 @@ namespace Develop03
     {
         static void Main(string[] args)
         {
+            //Set up first verse
             Reference nwReferenceFirst = new Reference();
+            Verse nwFirstVerse = new Verse();
             nwReferenceFirst.SetBook("Jeremiah");
             nwReferenceFirst.SetChapter(4);
             nwReferenceFirst.SetVerse(19);
             string nwFirstStr = "My bowels, my bowels! I am pained at my very heart; ";
             nwFirstStr += "my heart maketh a noise in me; I cannot hold my peace, because thou hast heard ";
             nwFirstStr += "O my soul, the sound of the trumpet, the alarm of war.";
-            List<string> nwFirstStrings = nwFirstStr.Split(" ").ToList();
-            List<Word> nwFirstWords = new List<Word>();
-            foreach (string nwstring in nwFirstStrings)
-            {
-                Word newWord = new Word();
-                newWord.SetWord(nwstring);
-                nwFirstWords.Add(newWord);
-            }
+            nwFirstVerse.SetReference(nwReferenceFirst);
+            nwFirstVerse.SetWords(nwFirstStr);
 
+            //Set up second verse
             Reference nwReferenceSecond = new Reference();
+            Verse nwSecondVerse = new Verse();
             nwReferenceSecond.SetBook("Jeremiah");
             nwReferenceSecond.SetChapter(4);
             nwReferenceSecond.SetVerse(20);
+            string nwSecondStr = "Destruction upon destruction is cried; for the whole land is spoiled:";
+            nwSecondStr += "suddenly are my tents spoiled, and my curtains in a moment.";
+            nwSecondVerse.SetReference(nwReferenceSecond);
+            nwSecondVerse.SetWords(nwSecondStr);
         }
     }
 }
--- prove/Develop03/Verse.cs
+++ prove/Develop03/Verse.cs
@@ -4,8 +4,27 @@ namespace Develop03
 {
     public class Verse
     {
-        public Verse(Reference _reference, List<Word> _words)
+        Reference _reference;
+        List<string> _stringList = new List<string>();
+        List<Word> _wordList = new List<Word>();
+
+        public Verse()
         {}
-        
+
+        public void SetReference(Reference nwReference)
+        {
+            _reference = nwReference;
+        }
+
+        public void SetWords(string words)
+        {
+            _stringList = words.Split(" ").ToList();
+            foreach (string nwString in _stringList)
+            {
+                Word nwWord = new Word();
+                nwWord.SetWord(nwString);
+                _wordList.Add(nwWord);
+            }
+        }
     }
 }
