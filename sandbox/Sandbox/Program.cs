c1e5c360b2e99371c2f39948a76d36a84de93450
Noah Weston <weston4ever@gmail.com>
2023-06-21 09:51:09 -0600

    try again

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,23 @@
 using System;
 
-class Program
+namespace sandbox
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Sandbox World!");
+        static void Main(string[] args)
+        {
+            Dictionary<string, Command> commands = new Dictionary<string, Command>();
+            commands["breathe"] = new StartBreathing();
+            commands["list"] = new StartListing();
+            bool wtf = true;
+
+            while (wtf == true)
+            {
+                Console.WriteLine("What do you want to do");
+                string response = Console.ReadLine();
+                Command command = commands[response];
+                command.Execute();
+            }
+        }
     }
 }
---
+++ sandbox/Sandbox/StartBreathing.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace sandbox
+{
+    public class StartBreathing:Command
+    {
+        public override void Execute()
+        {
+            Console.WriteLine("Executing breathing");
+        }
+    }
+}
